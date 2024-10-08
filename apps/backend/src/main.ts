b4f3662c18dbe7c628b27a35255c631483fc07fe
MilanKovacic <9189985+MilanKovacic@users.noreply.github.com>
2024-10-08 05:09:22 +0200

    import nest library in nest application

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -7,6 +7,7 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { BackendLibraryModule } from '@backend/library';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +18,7 @@ async function bootstrap() {
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
+  console.log(BackendLibraryModule);
 }
 
 bootstrap();
